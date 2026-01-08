f921e6fee93c3b1b871f10428a4a96ed38d24450
Kyson <chenqixin.life@gmail.com>
2026-01-08 20:08:00 +0800

    Update emby_save_link.js

--- emby_save_link.js
+++ emby_save_link.js
@@ -14,7 +14,9 @@ let videoKey = videoIdMatch ? videoIdMatch[1] : null;
 // 只有在 Range 是 0- 且 VPS 成功返回 307 时才记录
 console.log(`ID: ${videoKey} range:${range} status:${status} location:${location}`);
 if (videoKey && range === "bytes=0-" && (status == 307) && location) {
+    console.log(`save start`)
     $prefs.setValueForKey(location, "emby_115_" + videoKey);
+    console.log(`save end`)
     console.log(`[Emby优化] 成功捕获直链 ID: ${videoKey}, Location: ${location}`);
 }
 
